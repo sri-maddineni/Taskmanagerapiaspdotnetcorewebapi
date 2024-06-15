1129cc3923c5f46fa7989eb7112e73b2f25f826b
iTKonnects-connectwithtechnology <itkonnects@gmail.com>
2024-06-15 13:13:01 +0530

    first commit and basic get all records and delete all done

---
+++ Mongocrud/Mongocrud/Program.cs
@@ -0,0 +1,31 @@
+using Mongocrud.DataAccessLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//this project video tutorials
+
+builder.Services.AddScoped<ICrudDL, CrudDL>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
